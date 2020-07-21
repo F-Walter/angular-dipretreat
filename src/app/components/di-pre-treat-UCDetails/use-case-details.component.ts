7fcf56269bcd89ae7a7b96a65f325e086a749d72
Walter <walter.forcignano@gmail.com>
2020-07-21 15:35:19 +0200

    Modified theme  and table in uc details

--- src/app/components/di-pre-treat-UCDetails/use-case-details.component.ts
+++ src/app/components/di-pre-treat-UCDetails/use-case-details.component.ts
@@ -3,16 +3,16 @@ import { Component, OnInit } from '@angular/core';
 
 //For now values in the db are provided statically, later will be provided from a service
 const ELEMENT_DATA = [
-  {state: 1, item: 'Hydrogen', hour: 1.0079, problem: 'H'},
-  {state: 2, item: 'Helium', hour: 4.0026, problem: 'He'},
-  {state: 3, item: 'Lithium', hour: 6.941, problem: 'Li'},
-  {state: 4, item: 'Beryllium', hour: 9.0122, problem: 'Be'},
-  {state: 5, item: 'Boron', hour: 10.811, problem: 'B'},
-  {state: 6, item: 'Carbon', hour: 12.0107, problem: 'C'},
-  {state: 7, item: 'Nitrogen', hour: 14.0067, problem: 'N'},
-  {state: 8, item: 'Oxygen', hour: 15.9994, problem: 'O'},
-  {state: 9, item: 'Fluorine', hour: 18.9984, problem: 'F'},
-  {state: 10, item: 'Neon', hour: 20.1797, problem: 'Ne'},
+  {state: 1, item: 'Hydrogen', hour: 1.00, problem: 'H'},
+  {state: 2, item: 'Helium', hour: 4.00, problem: 'He'},
+  {state: 3, item: 'Lithium', hour: 6.94, problem: 'Li'},
+  {state: 4, item: 'Beryllium', hour: 9.01, problem: 'Be'},
+  {state: 5, item: 'Boron', hour: 10.51, problem: 'B'},
+  {state: 6, item: 'Carbon', hour: 12.01, problem: 'C'},
+  {state: 7, item: 'Nitrogen', hour: 14.00, problem: 'N'},
+  {state: 8, item: 'Oxygen', hour: 15.33, problem: 'O'},
+  {state: 9, item: 'Fluorine', hour: 18.38, problem: ''},
+  {state: 10, item: 'Neon', hour: 20.17, problem: ''},
 ];
 
 @Component({
