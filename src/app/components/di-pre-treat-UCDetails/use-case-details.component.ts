16614285284ef9c877fbcdee3d0c0867a2481f80
Walter <walter.forcignano@gmail.com>
2020-07-23 09:29:03 +0200

    Modified table and data

--- src/app/components/di-pre-treat-UCDetails/use-case-details.component.ts
+++ src/app/components/di-pre-treat-UCDetails/use-case-details.component.ts
@@ -1,34 +1,129 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 
 //For now values in the db are provided statically, later will be provided from a service
-const ELEMENT_DATA = [
-  {state: 1, item: 'Hydrogen', hour: 1.00, problem: 'H'},
-  {state: 2, item: 'Helium', hour: 4.00, problem: 'He'},
-  {state: 3, item: 'Lithium', hour: 6.94, problem: 'Li'},
-  {state: 4, item: 'Beryllium', hour: 9.01, problem: 'Be'},
-  {state: 5, item: 'Boron', hour: 10.51, problem: 'B'},
-  {state: 6, item: 'Carbon', hour: 12.01, problem: 'C'},
-  {state: 7, item: 'Nitrogen', hour: 14.00, problem: 'N'},
-  {state: 8, item: 'Oxygen', hour: 15.33, problem: 'O'},
-  {state: 9, item: 'Fluorine', hour: 18.38, problem: ''},
-  {state: 10, item: 'Neon', hour: 20.17, problem: ''},
-];
+
+export interface Item {
+  state: number; // 0 nessun problema // 1 problema // 2 problema risolto
+  id: string;
+  hour: number;
+  problemsFound: string;
+  description: string;
+}
 
 @Component({
   selector: 'app-use-case-details',
   templateUrl: './use-case-details.component.html',
-  styleUrls: ['./use-case-details.component.css']
+  styleUrls: ['./use-case-details.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class UseCaseDetailsComponent implements OnInit {
-
-  displayedColumns: string[] = ['state', 'item','hour', 'problem','button'];
+export class UseCaseDetailsComponent {
   dataSource = ELEMENT_DATA;
+  columnsToDisplay = ['state', 'id', 'hour', 'problemsFound'];
+  expandedElement: Item | null;
 
-  panelOpenState:boolean
-  constructor() { }
+  headerOfColumn(column: string) {
+    switch (column) {
+      case 'state':
+        return 'Stato';
+      case 'id':
+        return 'Componente';
+      case 'hour':
+        return 'Ora';
+      case 'problemsFound':
+        return 'Problemi Rilevati';
+      default:
+        break;
+    }
 
-  ngOnInit(): void {
   }
-
 }
+
+const ELEMENT_DATA: Item[] = [
+  {
+    state: 0,
+    id: 'Hydrogen',
+    hour: 1.0079,
+    problemsFound: '',
+    description: `Hydrogen is a chemical element with problemsFound H and atomic number 1. With a standard
+        atomic hour of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    state: 1,
+    id: 'Helium',
+    hour: 4.0026,
+    problemsFound: 'Tipologia Errore',
+    description: `Helium is a chemical element with problemsFound He and atomic number 2. It is a
+        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+        group in the periodic table. Its boiling point is the lowest among all the elements.`
+  }, {
+    state: 2,
+    id: 'Lithium',
+    hour: 6.941,
+    problemsFound: 'Tipologia Errore - risolto',
+    description: `Lithium is a chemical element with problemsFound Li and atomic number 3. It is a soft,
+        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
+        lightest solid element.`
+  }, {
+    state: 0,
+    id: 'Beryllium',
+    hour: 9.0122,
+    problemsFound: '',
+    description: `Beryllium is a chemical element with problemsFound Be and atomic number 4. It is a
+        relatively rare element in the universe, usually occurring as a product of the spallation of
+        larger atomic nuclei that have collided with cosmic rays.`
+  }, {
+    state: 0,
+    id: 'Boron',
+    hour: 10.811,
+    problemsFound: '',
+    description: `Boron is a chemical element with problemsFound B and atomic number 5. Produced entirely
+        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
+        low-abundance element in the Solar system and in the Earth's crust.`
+  }, {
+    state: 1,
+    id: 'Carbon',
+    hour: 12.0107,
+    problemsFound: 'Errore',
+    description: `Carbon is a chemical element with problemsFound C and atomic number 6. It is nonmetallic
+        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
+        to group 14 of the periodic table.`
+  }, {
+    state: 2,
+    id: 'Nitrogen',
+    hour: 14.0067,
+    problemsFound: 'Errore risolto',
+    description: `Nitrogen is a chemical element with problemsFound N and atomic number 7. It was first
+        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
+  }, {
+    state: 1,
+    id: 'Oxygen',
+    hour: 15.9994,
+    problemsFound: 'Errore',
+    description: `Oxygen is a chemical element with problemsFound O and atomic number 8. It is a member of
+         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
+         agent that readily forms oxides with most elements as well as with other compounds.`
+  }, {
+    state: 0,
+    id: 'Fluorine',
+    hour: 18.9984,
+    problemsFound: '',
+    description: `Fluorine is a chemical element with problemsFound F and atomic number 9. It is the
+        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
+        conditions.`
+  }, {
+    state: 1,
+    id: 'Neon',
+    hour: 20.1797,
+    problemsFound: 'Errore',
+    description: `Neon is a chemical element with problemsFound Ne and atomic number 10. It is a noble gas.
+        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
+        two-thirds the density of air.`
+  },
+];
